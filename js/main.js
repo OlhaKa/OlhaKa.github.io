cdbaf85305f2150fcaba919d3cef1cccb169c8ca
olga.kolomiiets <karazhakovao@gmail.com>
2019-01-10 20:24:49 +0200

    media styles

--- js/main.js
+++ js/main.js
@@ -21,6 +21,15 @@ $(document).ready(function(){
             }
          });
 
+         $(document).click(function (event) {
+            var clickover = $(event.target);
+            var $navbar = $(".navbar-collapse");               
+            var _opened = $navbar.hasClass("show");
+            if (_opened === true && !clickover.hasClass("navbar-toggle")) {      
+                $navbar.collapse('hide');
+            }
+        });
+
         //  var vh = $( window ).height();
         //     $('.section').css('height', vh);
 
